cc6b919ba9a45b8815a47e7c292418790885b815
theskyworld <2456450687@qq.com>
2023-05-26 20:14:07 +0800

    add comments for emit

--- src/runtime-core/componentEmits.ts
+++ src/runtime-core/componentEmits.ts
@@ -3,7 +3,9 @@ export function emit(instance, eventName, ...args) { //args接收其它参数（
 
     const { props } = instance;
 
+    // add→ Add   add-num → Add-num
     const capitalizeFirstLetter = (key: string) => key.charAt(0).toUpperCase() + key.slice(1);
+    // onAdd  onAdd-num
     const handleKey = (key: string) => key ? "on" + capitalizeFirstLetter(key) : "";
 
 
@@ -14,10 +16,32 @@ export function emit(instance, eventName, ...args) { //args接收其它参数（
             return c ? c.toUpperCase() : "";
         });
     }
+    // onAdd onAddNum
     const handlerName = handleKey(camelize(eventName));
+    // 根据转换后的eventName从props中获取对应属性的值（根据事件名获取对应事件的回调函数）
+    // 即传入时（转换前）：
+    //  emit('add', 1, 2);
+    //  emit('add-num', 1, 2);
+    // "add" "add-num"
+    // 转换后
+    // onAdd onAddNum
+    // 从Props中获取对应的事件回调函数
+    // props
+    // {
+    //     onAdd(a, b, c) {
+    //         console.log('onAdd', a, b, c)
+    //     },
+    //     // 即
+    //     // onAdd = (a, b, c) => console.log('onAdd', a, b, c)
+    //     onAddNum(a, b, c) {
+    //         console.log('onAddNum', a, b, c)
+    //     }
+    // }
     const handler = props[handlerName];
     
-    console.log(handlerName)
+    // console.log(handlerName)
+
+    // 执行回调函数
     // if (handler) {
     //     handler();
     // }
